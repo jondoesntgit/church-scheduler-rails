bafab507e3af9dbf4fbaa9c3e9e7b758ed9556a6
Jonathan Wheeler <jamwheel@Jonathans-MacBook-Pro.local>
2019-09-30 17:35:47 -0700

    Added starting skeleton for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,14 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  #get 'events/index'
+  root 'welcome#index'
+  
+  resources :events
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v0, defaults: { format: 'json' } do
     get 'events', to: 'events#index'
   end
+
 end
